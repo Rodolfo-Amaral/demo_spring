d4c137137ca168cb8ec019e6540595e2587e520b
Rodolfo J. C. Amaral <rodolfocornieri@gmail.com>
2021-05-16 16:42:16 -0300

    Services

--- src/main/java/com/rodolfoamaral/demo/resources/ResourceUsuario.java
+++ src/main/java/com/rodolfoamaral/demo/resources/ResourceUsuario.java
@@ -2,22 +2,35 @@
 
 package com.rodolfoamaral.demo.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rodolfoamaral.demo.entities.Usuario;
+import com.rodolfoamaral.demo.services.ServicesUsuario;
 
 @RestController //indicar que essa classe é um recurso web
 @RequestMapping(value = "/usuarios")
 public class ResourceUsuario {
 	
+	@Autowired //injetar o service do usuario automaticamente
+	private ServicesUsuario servico;  //dependencia para o serviceUsuario
+	
 	@GetMapping //responde a requisição do tipo "get"
-	public ResponseEntity<Usuario> localizarTodos(){ //retornar respostas de requisições web
-		Usuario u = new Usuario(1L, "Mario", "supermario@yahoo.com", "9999999", "123456");
-		return ResponseEntity.ok().body(u); //retornar a resposta
-		
+	public ResponseEntity<List<Usuario>> localizarTodos(){ //retornar respostas de requisições web
+		List<Usuario> lista = servico.localizaTodos();
+		return ResponseEntity.ok().body(lista); //retornar a resposta
 	}
-
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Usuario> localizarID(@PathVariable Long id){
+		Usuario obj = servico.localizarID(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
---
+++ src/main/java/com/rodolfoamaral/demo/services/ServicesUsuario.java
@@ -0,0 +1,29 @@
+package com.rodolfoamaral.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rodolfoamaral.demo.entities.Usuario;
+import com.rodolfoamaral.demo.repositories.RepositoryUsuario;
+
+@Service
+public class ServicesUsuario {
+	
+	@Autowired
+	private RepositoryUsuario repoUsuario;
+	
+	//RETORNAR TODOS USUARIOS DO BANCO
+	public List<Usuario> localizaTodos() {
+		return repoUsuario.findAll();
+	}
+	
+	//BUSCAR USER PELA ID
+	public Usuario localizarID(Long id) {
+		Optional<Usuario> obj = repoUsuario.findById(id);
+		return obj.get(); //retornar o obj dentro do optional
+	}
+
+}
