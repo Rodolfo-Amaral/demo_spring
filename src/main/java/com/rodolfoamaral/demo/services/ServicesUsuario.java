d8b4817798ca3375291a94f2059b74464fcb6a27
Rodolfo J. C. Amaral <rodolfocornieri@gmail.com>
2021-05-17 20:45:23 -0300

    Tratamento de Exceções 3

--- src/main/java/com/rodolfoamaral/demo/services/ServicesUsuario.java
+++ src/main/java/com/rodolfoamaral/demo/services/ServicesUsuario.java
@@ -3,6 +3,8 @@ package com.rodolfoamaral.demo.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -15,44 +17,48 @@ import com.rodolfoamaral.demo.services.exceptions.ResourceNotFoundExcecao;
 
 @Service
 public class ServicesUsuario {
-	
+
 	@Autowired
 	private RepositoryUsuario repoUsuario;
-	
-	//RETORNAR TODOS USUARIOS DO BANCO
+
+	// RETORNAR TODOS USUARIOS DO BANCO
 	public List<Usuario> localizaTodos() {
 		return repoUsuario.findAll();
 	}
-	
-	//BUSCAR USER PELA ID
+
+	// BUSCAR USER PELA ID
 	public Usuario localizarID(Long id) {
 		Optional<Usuario> obj = repoUsuario.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundExcecao(id));
 	}
-	
+
 	public Usuario inserirUsuario(Usuario obj) {
 		return repoUsuario.save(obj);
 	}
-	
+
 	public void deletarUsuario(Long id) {
 		try {
 			repoUsuario.deleteById(id);
-		} catch(EmptyResultDataAccessException e) {
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundExcecao(id);
-		}catch(DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DBException(e.getMessage());
 		}
 	}
 
 	public Usuario atualizarUsuario(Long id, Usuario obj) {
-		Usuario user = repoUsuario.getOne(id);
-		atualizarDados(user, obj);
-		return repoUsuario.save(user);		
+		try {
+			Usuario user = repoUsuario.getOne(id);
+			atualizarDados(user, obj);
+			return repoUsuario.save(user);		
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundExcecao(id);
+		}
 	}
 
 	private void atualizarDados(Usuario user, Usuario obj) {
 		user.setNome(obj.getNome());
 		user.setEmail(obj.getEmail());
-		user.setFone(obj.getFone());		
+		user.setFone(obj.getFone());
 	}
 }
